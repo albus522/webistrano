88fef134829deec4d27549cbe60f313e6eae63c4
David Genord II <david@xspond.com>
2012-03-20 17:26:13 -0400

    Updated test_helper require

--- test/unit/notification_test.rb
+++ test/unit/notification_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require File.expand_path('../../test_helper', __FILE__)
 
 class NotificationTest < ActiveSupport::TestCase
   FIXTURES_PATH = File.dirname(__FILE__) + '/../fixtures'
